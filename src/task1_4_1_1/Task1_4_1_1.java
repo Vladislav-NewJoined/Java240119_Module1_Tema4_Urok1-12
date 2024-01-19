d15b60c3e8720f70eab8f9ad3592154f8cee8ff2
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-01-19 13:28:38 +0300

    Initial Commit

--- src/task1_4_1_1/Task1_4_1_1.java
+++ src/task1_4_1_1/Task1_4_1_1.java
@@ -3,16 +3,14 @@ package task1_4_1_1;
 public class Task1_4_1_1 {
     public static void main(String[] args) {
         System.out.println("""
-            Задание:\s
-            Модуль 1. Тема 4. Урок 1. Наследование
-                Задание 1. Реализуйте классы с наследованием: директория, файл\s
+                Задание:\s
+                Модуль 1. Тема 4. Урок 1. Наследование
+                    Задание 1. Реализуйте классы с наследованием: директория, файл\s
 
-            Решение:\s""");
+                Решение:\s""");
 
         Task1_4_1_1_DirectoryAndFileExtended data = new Task1_4_1_1_DirectoryAndFileExtended();
-//        data.directory = "directory_For_testFile";
         data.directory = "src/task1_4_1_1/directory_For_testFile";
-//        data.file = "directory_For_testFile/testFile.txt";
         data.file = "src/task1_4_1_1/directory_For_testFile/testFile.txt";
 
         data.printDirectory();
--- src/task1_4_1_3/Task1_4_1_3.java
+++ src/task1_4_1_3/Task1_4_1_3.java
@@ -1,23 +1,14 @@
 package task1_4_1_3;
 
-// источник: iTunes Search API Constructings Searches - контекст для поиска
-// источник: https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/SearchExamples.html#//apple_ref/doc/uid/TP40017632-CH6-SW1
-// источник: вот этот пример скачали:  https://itunes.apple.com/search?term=jack+johnson&limit=25
-// источник: далее сюда возвращаемый ответ вставили: https://codebeautify.org/jsonviewer (приложен файл 1.txt)
-
-// прервался на мин 12 34
-// мин 14 56 это наследование
-// домашку начинать делать с мин 18 54
-
 public class Task1_4_1_3 {
     public static void main(String[] args) {
         System.out.println("""
-            Задание:\s
-            Модуль 1. Тема 4. Урок 1. Наследование
-                Задание 3. Реализуйте классы с наследованием: персонаж, главный герой, злодей,
-                друг главного героя\s
+                Задание:\s
+                Модуль 1. Тема 4. Урок 1. Наследование
+                    Задание 3. Реализуйте классы с наследованием: персонаж, главный герой, злодей,
+                    друг главного героя\s
 
-            Решение:\s""");
+                Решение:\s""");
 
         Task1_4_1_3_Movie movie = new Task1_4_1_3_Movie();
         movie.personage = "The personage, his name is John";
